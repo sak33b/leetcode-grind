4f045cd44484f16cd38ef56a5759deab5ed2f8f5
sak33b <89086878+sak33b@users.noreply.github.com>
2025-04-27 03:31:49 +0600

    add implementation for "Insert into a Binary Search Tree" problem

---
+++ binary-search-tree/701.insert-into-a-binary-search-tree.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=701 lang=java
+ *
+ * [701] Insert into a Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        } else if (val > root.val) {
+            root.right = insertIntoBST(root.right, val);
+        } else if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+}
+// @lc code=end
+
