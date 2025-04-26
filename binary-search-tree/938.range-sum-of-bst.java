9ac0be486af79f978db8c32cc4e214606fff8c0d
sak33b <89086878+sak33b@users.noreply.github.com>
2025-04-27 05:38:20 +0600

    add implementation for "Range Sum of BST" problem

---
+++ binary-search-tree/938.range-sum-of-bst.java
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode id=938 lang=java
+ *
+ * [938] Range Sum of BST
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum = 0;
+
+        while (root.val >= low) {
+            sum += root.val;
+
+            if (low > root.val) {
+                root = root.right;
+            } else {
+                root = root.left;
+            }
+        }
+    }
+}
+// @lc code=end
+
